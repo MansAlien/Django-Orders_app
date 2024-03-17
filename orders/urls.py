45bce74a3b2ca8f644afda5c5bc52f51cdb7690a
MansAlien <mans012033945421@gmail.com>
2024-03-18 01:26:56 +0200

    htmx test 'it works :)'

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from .views import CatigoryListView
+from .views import CatigoryListView, view_categories 
 
 urlpatterns = [
     path("", CatigoryListView.as_view(), name="home"),
 ]
+
+
+htmx_urlpatterns = [
+    path("view_categories/", view_categories, name="view_categories"),
+]
+
+urlpatterns += htmx_urlpatterns
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,13 @@
 from django.views.generic import ListView
 from .models import Catigory
+from django.http.response import HttpResponse 
+from django_htmx.http import HttpResponseClientRedirect
+from django.shortcuts import render
 
 class CatigoryListView(ListView):
     model = Catigory
     template_name = "home.html"
+
+
+def view_categories(request):
+    return HttpResponse("Hello there")
