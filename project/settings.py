a92210117903a098dd170dc204743aad75562249
MansAlien <mans012033945421@gmail.com>
2024-03-18 00:38:22 +0200

    adding htmx

--- project/settings.py
+++ project/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     # my app
     "accounts.apps.AccountsConfig",
     "orders.apps.OrdersConfig",
+    "django_htmx",
 ]
 
 MIDDLEWARE = [
@@ -64,6 +65,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django_htmx.middleware.HtmxMiddleware",
 ]
 
 ROOT_URLCONF = 'project.urls'
