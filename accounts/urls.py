f0913c05f3752d68e454c6de9e89ea454c7a1349
MansAlien <mans012033945421@gmail.com>
2024-04-30 23:44:50 +0300

    create the tabs of the employee_detail

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,37 +1,47 @@
-from django.urls import path
-from .views import SignUpView, employee_view, employee_detail_view, create_user, table_refresh, user_profile, cards,  user_update_view, user_update_profile, info_refresh, reset_password, create_deduction_view, deduction_refresh, permission_view, EmployeeView
 from django.contrib.auth import views as auth_views
+from django.urls import path
+
+from .views import (
+    EmployeeView,
+    SignUpView,
+    cards,
+    create_deduction_view,
+    create_user,
+    deduction,
+    employee_detail_view,
+    info,
+    permission_view,
+    reset_password,
+    table_refresh,
+    user_profile,
+    user_update_profile,
+    user_update_view,
+)
 
 urlpatterns = [
-    path('employee_list/', EmployeeView.as_view(), name="employee_list"),
-    path('employee_list/<int:pk>', employee_detail_view, name="employee_detail"),
-
-
-
+    path("employee_list/", EmployeeView.as_view(), name="employee_list"),
+    path("employee_list/<int:pk>", employee_detail_view, name="employee_detail"),
     # Password reset links (refers to django.contrib.auth.views)
-    path('signup/', SignUpView.as_view(), name='signup'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path("signup/", SignUpView.as_view(), name="signup"),
+    path( "password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"),
+    path( "password_reset/done/", auth_views.PasswordResetDoneView.as_view(), name="password_reset_done",),
+    path( "reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm",),
+    path( "reset/done/", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete",),
     # for the admin
-    path('reset_password/<int:user_id>/', reset_password, name='reset_password'),
+    path("reset_password/<int:user_id>/", reset_password, name="reset_password"),
 ]
 
 htmx_urlpatterns = [
-    path('create_user/', create_user, name='create_user'),
-    path('employee_list/<int:pk>/edit_user/', user_update_view, name='edit_user'),
-    path('employee_list/<int:pk>/edit_user_profile/', user_update_profile, name='edit_user_profile'),
-    path('user_profile/<int:pk>', user_profile, name='user_profile'),
-    path('deduction/<int:pk>', create_deduction_view, name='create_deduction'),
-    path('permissions/<int:pk>', permission_view, name='permissions'),
-
-
-    path('info/<int:pk>/', info_refresh, name='info_refresh'),
-    path('deduction_refresh/<int:pk>/', deduction_refresh, name='deduction_refresh'),
-    path('table_refresh/', table_refresh, name='table_refresh'),
-    path('cards/', cards, name='cards'),
-
+    path("create_user/", create_user, name="create_user"),
+    path("employee_list/<int:pk>/edit_user/", user_update_view, name="edit_user"),
+    path( "employee_list/<int:pk>/edit_user_profile/", user_update_profile, name="edit_user_profile",),
+    path("user_profile/<int:pk>", user_profile, name="user_profile"),
+    path("deduction/<int:pk>", create_deduction_view, name="create_deduction"),
+    path("permissions/<int:pk>", permission_view, name="permissions"),
+    path("info/<int:pk>/", info, name="info"),
+    path("deduction_refresh/<int:pk>/", deduction, name="deduction"),
+    path("table_refresh/", table_refresh, name="table_refresh"),
+    path("cards/", cards, name="cards"),
 ]
 
 urlpatterns += htmx_urlpatterns
--- accounts/views.py
+++ accounts/views.py
@@ -1,52 +1,62 @@
-from django.shortcuts import render, redirect 
-from django.urls import reverse_lazy
-from django.views.generic import  TemplateView, ListView
-from django.views.generic.edit import  CreateView, UpdateView
-from login_history.models import LoginHistory
-from django.http import HttpResponse
-from django.contrib.auth.models import User
-from .models import UserProfile, JobTitleHistory, SalaryHistory, Deduction
-from .forms import UserCreationForm, UserProfileForm, UserUpdateForm, PasswordResetForm, DeductionForm
-from django.http import JsonResponse
-from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404
-from .forms import PermissionForm
-from django.contrib.auth.models import Permission
-from django.db.models import Q
 from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.models import Permission, User
+from django.db.models import Q
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, TemplateView
+from django.views.generic.edit import CreateView, UpdateView
+
+from login_history.models import LoginHistory
+
+from .forms import (
+    DeductionForm,
+    PasswordResetForm,
+    PermissionForm,
+    UserCreationForm,
+    UserProfileForm,
+    UserUpdateForm,
+)
+from .models import Deduction, JobTitleHistory, SalaryHistory, UserProfile
+
 
 ##########################################################
 #######                Forms                ##############
 ##########################################################
 class SignUpView(CreateView):
-   form_class = UserCreationForm
-   success_url = reverse_lazy("admin:index")
-   template_name = "registration/signup.html"
+    form_class = UserCreationForm
+
+
+success_url = reverse_lazy("admin:index")
+template_name = "registration/signup.html"
+
 
 @permission_required("accounts.add_userprofile")
 def create_user(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save() 
-            return redirect('user_profile', pk=user.id)
+            user = form.save()
+            return redirect("user_profile", pk=user.id)
     else:
         form = UserCreationForm()
-    return render(request, 'settings/employee/modals/create_user.html', {'form': form})
+    return render(request, "settings/employee/modals/create_user.html", {"form": form})
+
 
 @permission_required("accounts.change_userprofile")
 def user_update_view(request, pk):
     user_instance = get_object_or_404(User, pk=pk)
-    
-    if request.method == 'POST':
+
+    if request.method == "POST":
         form = UserUpdateForm(request.POST, instance=user_instance)
         if form.is_valid():
             form.save()
-            return HttpResponse(status=204, headers={'HX-Trigger': 'info'})
+            return HttpResponse(status=204, headers={"HX-Trigger": "info"})
     else:
         form = UserUpdateForm(instance=user_instance)
-        
-    return render(request, 'settings/employee/modals/edit_user.html', {'form': form})
+
+    return render(request, "settings/employee/modals/edit_user.html", {"form": form})
+
 
 @permission_required("accounts.add_userprofile")
 def user_profile(request, pk):
@@ -56,11 +66,17 @@ def user_profile(request, pk):
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
-            return HttpResponse(status=204, headers={'HX-Trigger': 'table_refresh, cards'})
+            return HttpResponse(
+                status=204, headers={"HX-Trigger": "table_refresh, cards"}
+            )
             # Redirect or do whatever you need after the update
     else:
         form = UserProfileForm(instance=profile)
-    return render(request, 'settings/employee/modals/create_user_profile.html', {'form': form, 'pk': pk})
+    return render(
+        request,
+        "settings/employee/modals/create_user_profile.html",
+        {"form": form, "pk": pk},
+    )
 
 
 @permission_required("accounts.change_userprofile")
@@ -71,18 +87,19 @@ def user_update_profile(request, pk):
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
-            return HttpResponse(status=204, headers={'HX-Trigger': 'info'})
+            return HttpResponse(status=204, headers={"HX-Trigger": "info"})
     else:
         form = UserProfileForm(instance=profile)
-    return render(request, 'settings/employee/modals/edit_user.html', {'form': form})
+    return render(request, "settings/employee/modals/edit_user.html", {"form": form})
+
 
 @permission_required("accounts.change_userprofile")
 def reset_password(request, user_id):
     user = User.objects.get(pk=user_id)
-    if request.method == 'POST':
+    if request.method == "POST":
         form = PasswordResetForm(request.POST)
         if form.is_valid():
-            new_password = form.cleaned_data['new_password']
+            new_password = form.cleaned_data["new_password"]
             # Reset the password for the user
             user.set_password(new_password)
             user.save()
@@ -90,75 +107,66 @@ def reset_password(request, user_id):
             return HttpResponse(status=204)
     else:
         form = PasswordResetForm()
-    return render(request, 'settings/employee/modals/edit_user.html', {'form': form})
+    return render(request, "settings/employee/modals/edit_user.html", {"form": form})
+
 
 @permission_required("accounts.change_userprofile")
 def create_deduction_view(request, pk):
     user = User.objects.get(id=pk)
     profile = UserProfile.objects.get(user=user)
-    if request.method == 'POST':
+    if request.method == "POST":
         form = DeductionForm(request.POST)
         if form.is_valid():
             deduction = form.save(commit=False)
             deduction.user_profile = profile
             deduction.save()
-            return HttpResponse(status=204, headers={'HX-Trigger': 'deduction_refresh'})
+            return HttpResponse(status=204, headers={"HX-Trigger": "deduction_refresh"})
     else:
         form = DeductionForm()
-    return render(request, 'settings/employee/modals/create_deduction.html', {'form': form})
+    return render(
+        request, "settings/employee/modals/create_deduction.html", {"form": form}
+    )
+
 
 @permission_required("accounts.change_userprofile")
 def permission_view(request, pk):
     user = User.objects.get(id=pk)
     permissions = Permission.objects.filter(
-        content_type__app_label='accounts',
-        codename__endswith="_userprofile"
+        content_type__app_label="accounts", codename__endswith="_userprofile"
     ).exclude(codename__startswith="delete")
-    
-    if request.method == 'POST':
+
+    if request.method == "POST":
         form = PermissionForm(request.POST, instance=user, permissions=permissions)
         if form.is_valid():
             form.save()
-            return HttpResponse(status=204, headers={'HX-Trigger': 'permissions_refresh'})
+            return HttpResponse(
+                status=204, headers={"HX-Trigger": "permissions_refresh"}
+            )
     else:
         form = PermissionForm(instance=user, permissions=permissions)
-    
-    return render(request, 'settings/employee/tabs/permissions.html', {'form': form, 'user': user})
+
+    return render(
+        request, "settings/employee/tabs/permissions.html", {"form": form, "user": user}
+    )
+
+
 ##########################################################
 #######                Refresh              ##############
 ##########################################################
 
-@permission_required("accounts.view_userprofile")
-def info_refresh(request, pk):
-    user = UserProfile.objects.get(id=pk)
-    job_title_history = JobTitleHistory.objects.filter(user_profile=user)
-    salary_history = SalaryHistory.objects.filter(user_profile=user)
-    context = {
-            "user":user,
-            "job_title_history":job_title_history,
-            "salary_history":salary_history,
-    }
-    return render(request, "settings/employee/tabs/info.html", context)
-
-@permission_required("accounts.view_userprofile")
-def deduction_refresh(request, pk):
-    user = UserProfile.objects.get(id=pk)
-    deduction_list = Deduction.objects.filter(user_profile=user)
-    context = {
-            "user":user,
-            "deduction_list":deduction_list,
-    }
-    return render(request, "settings/employee/tabs/deduction.html", context)
 
 @permission_required("accounts.view_userprofile")
 def table_refresh(request):
     user_profile = UserProfile.objects.all()
-    logged_in_users = LoginHistory.objects.filter(is_logged_in=True).values_list('user__username', flat=True)
+    logged_in_users = LoginHistory.objects.filter(is_logged_in=True).values_list(
+        "user__username", flat=True
+    )
     context = {
         "userprofile_list": user_profile,
         "login_history": logged_in_users,
     }
-    return render(request, "settings/employee/tables/table.html", context) 
+    return render(request, "settings/employee/tables/table.html", context)
+
 
 @permission_required("accounts.view_userprofile")
 def cards(request):
@@ -168,45 +176,53 @@ def cards(request):
     admin_inactive = User.objects.filter(is_active=False).count()
     inactive = User.objects.filter(is_active=False, is_superuser=False).count()
 
-    admin_online = LoginHistory.objects.filter(is_logged_in=True).values_list('user__username', flat=True).count()
-    online = LoginHistory.objects.filter(is_logged_in=True, user__is_superuser=False).values_list('user__username', flat=True).count()
+    admin_online = (
+        LoginHistory.objects.filter(is_logged_in=True)
+        .values_list("user__username", flat=True)
+        .count()
+    )
+    online = (
+        LoginHistory.objects.filter(is_logged_in=True, user__is_superuser=False)
+        .values_list("user__username", flat=True)
+        .count()
+    )
 
     admin_offline = admin_active - admin_online
     offline = active - online
     context = {
-        "admin_active":admin_active,
-        "active":active,
-
-        "admin_inactive":admin_inactive,
-        "inactive":inactive,
-
-        "admin_online":admin_online,
-        "online":online,
-
-        "admin_offline":admin_offline,
-        "offline":offline,
+        "admin_active": admin_active,
+        "active": active,
+        "admin_inactive": admin_inactive,
+        "inactive": inactive,
+        "admin_online": admin_online,
+        "online": online,
+        "admin_offline": admin_offline,
+        "offline": offline,
     }
-    return render(request, "settings/employee/cards.html", context) 
-
-
+    return render(request, "settings/employee/cards.html", context)
 
 
 ##########################################################
 #######                Views                ##############
 ##########################################################
 
+
 @permission_required("accounts.view_userprofile")
 def employee_view(request):
     user_profile = UserProfile.objects.all()
-    logged_in_users = LoginHistory.objects.filter(is_logged_in=True).values_list('user__username', flat=True)
+    logged_in_users = LoginHistory.objects.filter(is_logged_in=True).values_list(
+        "user__username", flat=True
+    )
     context = {
         "userprofile_list": user_profile,
         "login_history": logged_in_users,
     }
-    return render(request, "settings/employee/employee.html", context) 
+    return render(request, "settings/employee/employee.html", context)
+
 
 class EmployeeView(TemplateView):
     template_name = "settings/employee/employee.html"
+
 
 @permission_required("accounts.view_userprofile")
 def employee_detail_view(request, pk):
@@ -215,10 +231,33 @@ def employee_detail_view(request, pk):
     salary_history = SalaryHistory.objects.filter(user_profile=user)
     login_history = LoginHistory.objects.filter(user=user.user)
     context = {
-            "user":user,
-            "job_title_history":job_title_history,
-            "salary_history":salary_history,
-            "login_history":login_history,
+        "user": user,
+        "job_title_history": job_title_history,
+        "salary_history": salary_history,
+        "login_history": login_history,
     }
-    return render(request, "settings/employee/employee_detail.html", context) 
+    return render(request, "settings/employee/employee_detail.html", context)
 
+
+@permission_required("accounts.view_userprofile")
+def info(request, pk):
+    user = UserProfile.objects.get(id=pk)
+    job_title_history = JobTitleHistory.objects.filter(user_profile=user)
+    salary_history = SalaryHistory.objects.filter(user_profile=user)
+    context = {
+        "user": user,
+        "job_title_history": job_title_history,
+        "salary_history": salary_history,
+    }
+    return render(request, "settings/employee/tabs/info.html", context)
+
+
+@permission_required("accounts.view_userprofile")
+def deduction(request, pk):
+    user = UserProfile.objects.get(id=pk)
+    deduction_list = Deduction.objects.filter(user_profile=user)
+    context = {
+        "user": user,
+        "deduction_list": deduction_list,
+    }
+    return render(request, "settings/employee/tabs/deduction.html", context)
