3938b4b426882b991991dbb37edfbe5447b28017
MansAlien <mans012033945421@gmail.com>
2024-05-09 10:18:56 +0300

    update the inventory database

--- orders/models.py
+++ orders/models.py
@@ -1,15 +1,9 @@
 from django.db import models
-from django.utils.text import slugify
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    slug = models.SlugField(unique=True, null=True, blank=True, editable=False)
     is_active = models.BooleanField(default=False)
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.name)
-        super().save(*args, **kwargs)
 
     def __str__(self) -> str:
         return self.name
@@ -17,11 +11,14 @@ class Category(models.Model):
 class Sub_Category(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True, null=True, blank=True, editable=False)
+    is_active = models.BooleanField(default=False)
 
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.name)
-        super().save(*args, **kwargs)
+    def __str__(self) -> str:
+        return self.name
+
+class Attribute(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self) -> str:
         return self.name
@@ -35,33 +32,26 @@ class Product(models.Model):
         OUT_OF_STOCK: "Out Of Stock",
     }
 
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    sub_category = models.ForeignKey(Sub_Category, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=100, unique=True)
-    slug = models.SlugField(unique=True, null=True, blank=True, editable=False)
-    description = models.TextField(null=True)
+    description = models.TextField(null=True, blank=True)
     is_active = models.BooleanField(default=False)
     is_countable = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True, editable=False)
     stock_status = models.CharField(max_length=3, choices=STOCK_STATUS, default=OUT_OF_STOCK)
 
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.name)
-        super().save(*args, **kwargs)
-
     def __str__(self) -> str:
         return self.name
 
-class Attribute(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(null=True)
-
-    def __str__(self) -> str:
-        return self.name
 
 class AttributeValue(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
     attribute_value = models.CharField(max_length=100)
     attribute = models.ForeignKey(Attribute, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.attribute.name} : {self.attribute_value}"
 
 class ProductLine(models.Model):
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
@@ -70,4 +60,7 @@ class ProductLine(models.Model):
     is_active = models.BooleanField(default=False)
     attribute_values = models.ManyToManyField(AttributeValue, related_name="attribute_values")
 
+    def __str__(self):
+        attribute_values_str = ', '.join(str(attr_value) for attr_value in self.attribute_values.all())
+        return f"{self.product.name} - {attribute_values_str}"
 
