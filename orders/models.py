a3c8f165f4b675303ac4a2125740f3af2d3fee2d
MansAlien <mans012033945421@gmail.com>
2024-05-09 15:11:54 +0300

    update the attrubutevalue database

--- orders/models.py
+++ orders/models.py
@@ -24,14 +24,6 @@ class Attribute(models.Model):
         return self.name
 
 class Product(models.Model):
-    IN_STOCK= "IS"
-    OUT_OF_STOCK= "OOS"
-
-    STOCK_STATUS = {
-        IN_STOCK: "In Stock",
-        OUT_OF_STOCK: "Out Of Stock",
-    }
-
     sub_category = models.ForeignKey(Sub_Category, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(null=True, blank=True)
@@ -39,7 +31,6 @@ class Product(models.Model):
     is_countable = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True, editable=False)
-    stock_status = models.CharField(max_length=3, choices=STOCK_STATUS, default=OUT_OF_STOCK)
 
     def __str__(self) -> str:
         return self.name
@@ -47,18 +38,19 @@ class Product(models.Model):
 
 class AttributeValue(models.Model):
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
-    attribute_value = models.CharField(max_length=100)
     attribute = models.ForeignKey(Attribute, on_delete=models.CASCADE)
+    attribute_value = models.CharField(max_length=100)
 
     def __str__(self) -> str:
         return f"{self.attribute.name} : {self.attribute_value}"
 
 class ProductLine(models.Model):
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    attribute_values = models.ManyToManyField(AttributeValue, related_name="attribute_values")
     price = models.DecimalField(decimal_places=2, max_digits=5)
     stock_qty = models.IntegerField(default=0)
+    min_stock_qty = models.IntegerField(default=1)
     is_active = models.BooleanField(default=False)
-    attribute_values = models.ManyToManyField(AttributeValue, related_name="attribute_values")
 
     def __str__(self):
         attribute_values_str = ', '.join(str(attr_value) for attr_value in self.attribute_values.all())
