f9ec3f3a511d99c77366aedfda47ed57c3d3539f
MansAlien <mans012033945421@gmail.com>
2024-05-10 20:56:34 +0300

    create the tabs of the inventory

--- orders/urls.py
+++ orders/urls.py
@@ -1,36 +1,26 @@
 from django.urls import path
-from .views import HomeListView, SettingsTemplateView, categorycreateview, category_view, categorydeleteview, categoryrenameview, categoryapplyview, categorycancelview, subcategoryview, subcategorycreateview, subcategoryrenameview, subcategoryapplyview, subcategorycancelview, subcategorydeleteview, inventory_view
 from orders import views
 
 urlpatterns = [
-    path("", HomeListView.as_view(), name="home"),
-    path("settings/", SettingsTemplateView.as_view(), name="settings"),
-    path("settings/subcategory/", subcategoryview, name="subcategory"),
-    path("settings/inventory/", inventory_view, name="inventory"),
+    path("", views.HomeListView.as_view(), name="home"),
+    path("settings/", views.SettingsTemplateView.as_view(), name="settings"),
+
+    path("settings/inventory/", views.inventory_view, name="inventory"),
+    path("settings/inventory/category", views.category_view, name="category"),
+    path("settings/inventory/attributes", views.attributes_view, name="attributes"),
+    path("settings/inventory/product", views.product_view, name="product"),
+    path("settings/inventory/product_line", views.product_line_view, name="product_line"),
 ]
 
 
 htmx_urlpatterns = [
-
-    path("settings/inventory/category", category_view, name="category"),
+    # category
     path("settings/inventory/category/create", views.create_category, name="create_category"),
     path("settings/inventory/category/<int:pk>/edit", views.edit_category, name="edit_category"),
+
+    # sub_category
     path("settings/inventory/sub_category/create", views.create_sub_category, name="create_sub_category"),
     path("settings/inventory/sub_category/<int:pk>/edit", views.edit_sub_category, name="edit_sub_category"),
-
-
-    path("settings/category/create", categorycreateview, name="create"),
-    path("settings/category/<int:pk>/rename/", categoryrenameview, name="rename"),
-    path("settings/category/<int:pk>/apply/", categoryapplyview, name="apply"),
-    path("settings/category/cancel/", categorycancelview, name="cancel"),
-    path("settings/category/<int:pk>/delete", categorydeleteview, name="delete"),
-    
-    
-    path("settings/subcategory/create_sub", subcategorycreateview, name="create_sub"),
-    path("settings/subcategory/<int:pk>/rename", subcategoryrenameview, name="rename_sub"),
-    path("settings/subcategory/<int:pk>/apply/", subcategoryapplyview, name="apply_sub"),
-    path("settings/subcategory/<int:pk>/cancel/", subcategorycancelview, name="cancel_sub"),
-    path("settings/subcategory/<int:pk>/delete/", subcategorydeleteview, name="delete_sub"),
 ]
 
 urlpatterns += htmx_urlpatterns
--- orders/views.py
+++ orders/views.py
@@ -11,7 +11,15 @@ class HomeListView(ListView):
     model = Category
     template_name = "home.html"
 
+@method_decorator(login_required, name='dispatch')
+class SettingsTemplateView(TemplateView):
+    template_name="settings/settings.html"
 
+##########################################
+######### Inventory Databases ############
+##########################################
+
+# Inventory
 def inventory_view(request):
     category_list = Category.objects.all()
     context = {
@@ -19,6 +27,16 @@ def inventory_view(request):
     }
     return render(request, "settings/inventory/inventory.html", context)
 
+# Category
+@login_required
+def category_view(request):
+    category_list = Category.objects.all()
+    sub_category_list = Sub_Category.objects.all()
+    context = {
+        "category_list":category_list ,
+        "sub_category_list":sub_category_list ,
+    }
+    return render(request, "settings/inventory/tabs/category.html", context)
 
 def create_category(request):
     if request.method == "POST":
@@ -41,6 +59,7 @@ def edit_category(request, pk):
         form = CategoryForm(instance=category)
     return render( request, "settings/inventory/modals/edit_category.html", {"form": form, "pk":pk})
 
+# SubCategory
 def create_sub_category(request):
     if request.method == "POST":
         form = SubCategoryForm(request.POST)
@@ -62,104 +81,15 @@ def edit_sub_category(request, pk):
         form = SubCategoryForm(instance=sub_category)
     return render( request, "settings/inventory/modals/edit_sub_category.html", {"form": form, "pk":pk})
 
-@login_required
-def category_view(request):
-    category_list = Category.objects.all()
-    sub_category_list = Sub_Category.objects.all()
-    context = {
-        "category_list":category_list ,
-        "sub_category_list":sub_category_list ,
-    }
-    return render(request, "settings/inventory/tabs/category.html", context)
-
-@method_decorator(login_required, name='dispatch')
-class SettingsTemplateView(TemplateView):
-    template_name="settings/settings.html"
+# Attributes
+def attributes_view(request):
+    return render(request, "settings/inventory/tabs/attributes.html")
 
 
+# product
+def product_view(request):
+    return render(request, "settings/inventory/tabs/product.html")
 
-@login_required
-def categorycreateview(request):
-    category_name = request.POST.get("category")
-    category = Category.objects.create(name=category_name)
-    category_list = Category.objects.all()
-    context = {"category_list":category_list}
-    return render(request, "settings/category/category.html", context)
-
-@login_required
-def categoryrenameview(request, pk):
-    category = Category.objects.get(id=pk)
-    context = {"category":category}
-    return render(request, "settings/category/category_edit.html", context)
-
-@login_required
-def categoryapplyview(request, pk):
-    category = Category.objects.get(id=pk)
-    category.name = request.POST.get("update") 
-    category.save()
-    category_list = Category.objects.all()
-    context = {"category_list":category_list}
-    return render(request, "settings/category/category.html", context) 
-
-@login_required
-def categorydeleteview(request, pk):
-    category = Category.objects.get(id=pk)
-    category.delete()
-    category_list = Category.objects.all()
-    context = {"category_list":category_list}
-    return render(request, "settings/category/category.html", context)
-
-@login_required
-def categorycancelview(request):
-    category_list = Category.objects.all()
-    context = {"category_list":category_list}
-    return render(request, "settings/category/category.html", context)
-
-@login_required
-def subcategoryview(request):
-    category_id = request.POST.get("category_id")
-    category = Category.objects.get(id=category_id)
-    subcategory_list = category.sub_category_set.all()
-    context = {"subcategory_list":subcategory_list}
-    return render(request, "settings/category/subcategory.html" , context) 
-
-@login_required
-def subcategorycreateview(request):
-    subcategory_name = request.POST.get("subcategory_name")
-    category_id = request.POST.get("category_id")
-    category = Category.objects.get(pk=category_id)
-    category.sub_category_set.create(category=category_id, name=subcategory_name)
-    subcategory_list = category.sub_category_set.all()
-    context = {"subcategory_list":subcategory_list}
-    return render(request, "settings/category/subcategory.html" , context) 
-
-@login_required
-def subcategoryrenameview(request, pk):
-    subcategory = Sub_Category.objects.get(id=pk)
-    context = {"subcategory":subcategory}
-    return render(request, "settings/category/subcategory_edit.html", context)
-
-@login_required
-def subcategoryapplyview(request, pk):
-    subcategory = Sub_Category.objects.get(id=pk)
-    subcategory.name = request.POST.get("update_sub") 
-    subcategory.save()
-    subcategory_list = Sub_Category.objects.filter(category=subcategory.category.id)
-    context = {"subcategory_list":subcategory_list}
-    return render(request, "settings/category/subcategory.html", context) 
-
-@login_required
-def subcategorydeleteview(request, pk):
-    subcategory = Sub_Category.objects.get(id=pk)
-    subcategory.delete()
-    subcategory_list = Sub_Category.objects.filter(category=subcategory.category.id)
-    context = {"subcategory_list":subcategory_list}
-    return render(request, "settings/category/subcategory.html", context) 
-
-@login_required
-def subcategorycancelview(request, pk):
-    subcategory = Sub_Category.objects.get(id=pk)
-    subcategory_list = Sub_Category.objects.filter(category=subcategory.category.id)
-    context = {"subcategory_list":subcategory_list}
-    return render(request, "settings/category/subcategory.html", context) 
-
+# product line
+def product_line_view(request):
+    return render(request, "settings/inventory/tabs/product_line.html")
