e14b1671f30470154127655346aeacff01727711
MansAlien <mans012033945421@gmail.com>
2024-05-14 23:07:34 +0300

    update the settings env

--- project/settings.py
+++ project/settings.py
@@ -83,8 +83,14 @@ STATICFILES_FINDERS = [
     "compressor.finders.CompressorFinder",
 ]
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
-database_url = env('DATABASE_URL') 
+database_url = os.environ.get("DATABASE_URL")
 DATABASES["default"] = dj_database_url.parse(database_url)
 
 AUTH_PASSWORD_VALIDATORS = [
